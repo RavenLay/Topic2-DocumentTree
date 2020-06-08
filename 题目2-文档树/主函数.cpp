817b9f561bfd88972da25b2b2cb34a99e9996aac
Sakura-demon <455002446@qq.com>
2020-06-08 16:54:53 +0800

    基本框架

--- 题目2-文档树/主函数.cpp
+++ 题目2-文档树/主函数.cpp
@@ -2,24 +2,25 @@
 
 int main()
 {
+	//功能一
+	Tree* T;
+	T->CreateTreeAndPrint(T);
+
 	//功能选择界面(待美化)
 	int flag = 0;
 	cout << "Please input your choice";
 	cin >> flag;
-	Tree* T;
-	if (flag==1)//功能一
-	{
-		T->CreateTreeAndPrint(T);
-	}
-	else if(flag == 2)//功能二
+
+	//功能执行
+	if(flag == 1)//功能二
 	{
 		T->Insert(T);
 	}
-	else if (flag == 3)//功能三
+	else if (flag == 2)//功能三
 	{
 		T->SearchAndPrintLable(T);
 	}
-	else if (flag == 4)//功能四
+	else if (flag == 3)//功能四
 	{
 		T->SearchAndPrintTxt(T);
 	}
--- 题目2-文档树/白靖.h
+++ 题目2-文档树/白靖.h
@@ -2,12 +2,14 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 class Tree
 {
-private://文档树的数据结构(变量、指针等)
-	char data;//数据
+private:
+	//文档树的数据结构(变量、指针等)
+	string data;//数据
 	vector<Tree*>TreeNode;//长度可变指针数组
 
 	//新增函数声明标记处
@@ -32,11 +34,16 @@ void Tree::CreateTreeAndPrint(Tree* &T)
 void Tree::SearchAndPrintTxt(Tree* &T)
 {
 	//选择全部输出还是输出含关键字的文本
-	if ()//全部
+	//功能选择界面(待美化)
+	int flag = 0;
+	cout << "Please input your choice";
+	cin >> flag;
+
+	if (flag == 1)//全部
 	{
 		//遍历输出各叶子结点是文本的文本内容
-	} 
-	else//含关键字的
+	}
+	else if (flag == 2)//含关键字的
 	{
 		//遍历匹配输出各叶子结点是含关键字的文本内容
 	}
